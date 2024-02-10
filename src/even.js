adf6390cf06a6fcd0448a3c34c247070ce426a6a
Михаил <135731810+Mixey000@users.noreply.github.com>
2024-02-10 21:19:51 +0300

    Update even.js

--- src/even.js
+++ src/even.js
@@ -9,22 +9,28 @@ console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 export default function startGame() {
   let count = 0;
   const max = 3;
+
   function isEven(num) {
     return num % 2 === 0;
   }
+
   while (count < max) {
     const result = Math.floor(Math.random() * 50);
     console.log(`Question: ${result}`);
-    const youranswer = readlineSync.question('Your answer: ');
 
-    if ((isEven(result) && youranswer === 'yes') || (!isEven(result) && youranswer === 'no')) {
+    const yourAnswer = readlineSync.question('Your answer: ');
+
+    if ((isEven(result) && yourAnswer === 'yes') || (!isEven(result) && yourAnswer === 'no')) {
       count += 1;
       console.log('Correct!');
     } else {
-      console.log(`${youranswer} is wrong answer ;(. Correct answer was ${(result % 2 === 0 ? 'yes' : 'no')}.`);
-      console.log(`Let's try again,  ${name}!`);
+      const correctAnswer = isEven(result) ? 'yes' : 'no';
+      console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${name}!`);
       count = 0;
       return;
     }
-  }console.log(`Congratulations, ${name}!`);
+  }
+
+  console.log(`Congratulations, ${name}!`);
 }
