bd6df9d44b37c1a0936afe91ea152838048c2380
Mikhail <Mixey000@mail.ru>
2024-02-10 20:42:40 +0300

    update gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -5,46 +5,45 @@ import greetUser from './cli.js';
 const name = greetUser();
 
 function getGreatestCommonDivisor(a, b) {
-  let divisor = 1;
-  for (let i = 1; i <= Math.min(a, b); i += 1) {
-    if (a % i === 0 && b % i === 0) {
-      divisor = i;
-    }
-  }
-  return divisor;
+let divisor = 1;
+for (let i = 1; i <= Math.min(a, b); i += 1) {
+if (a % i === 0 && b % i === 0) {
+divisor = i;
 }
-const result = getGreatestCommonDivisor();
+}
+return divisor;
+}
 
 function generateNumber() {
-  return Math.floor(Math.random() * 100);
+return Math.floor(Math.random() * 100);
 }
 
 console.log('Find the greatest common divisor of given numbers.');
 
 export default function startGameNod() {
-  let count = 0;
-  const max = 3;
+const maxAttempts = 3;
+let correctAnswersCount = 0;
 
-  while (count < max) {
-    const num1 = generateNumber();
-    const num2 = generateNumber();
+while (correctAnswersCount < maxAttempts) {
+const num1 = generateNumber();
+const num2 = generateNumber();
+const result = getGreatestCommonDivisor(num1, num2);
 
-    const randomNumber = `${num1} ${num2}`;
+console.log(`Question: ${num1} ${num2}`);
 
-    console.log(`Question: ${randomNumber}`);
+const userAnswer = readlineSync.question('Your answer: ');
 
-    const youranswer = readlineSync.question('Your answer: ');
-    //   let result = ;
-    if (Number(youranswer) === result) {
-      count += 1;
-      console.log('Correct!');
-      if (count === max) { console.log(`Congratulations, ${name}!`); }
-    } else {
-      console.log(`${youranswer} is wrong answer ;(. Correct answer was ${result}.`);
-      console.log(`Let's try again,  ${name}!`);
-      count = 0;
-      break;
-    }
+if (Number(userAnswer) === result) {
+  correctAnswersCount++;
+  console.log('Correct!');
+  if (correctAnswersCount === maxAttempts) {
+    console.log(`Congratulations, ${name}!`);
   }
-  return result;
+} else {
+  console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${result}.`);
+  console.log(`Let's try again, ${name}!`);
+  correctAnswersCount = 0;
+  break;
+}
+}
 }
